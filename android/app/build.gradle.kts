72a9aedff37d7d11802ec1c74e907a728169f308
Aric <aricvasaya10@gmail.com>
2025-07-04 18:59:02 +0530

    Add Auth Controller

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,12 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+
+}
+dependencies{
+    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
 
 android {
@@ -28,6 +34,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled true
     }
 
     buildTypes {
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:elective_course_management/Pages/MainActivity.dart';
-import 'package:elective_course_management/Pages/course_details_page.dart';
 import 'package:elective_course_management/Pages/login.dart';
 import 'package:elective_course_management/Pages/register.dart';
 import 'package:elective_course_management/Pages/searchPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
-    MaterialApp(
+    GetMaterialApp(
       title: "Elective Course Management",
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
