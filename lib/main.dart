563b289a49010447cf36cb3bf6ea3ef213d2807f
Aric <aricvasaya10@gmail.com>
2025-07-04 19:37:38 +0530

    Add some main.dart fileing

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:elective_course_management/Controller/Auth_controller.dart';
 import 'package:elective_course_management/Pages/MainActivity.dart';
 import 'package:elective_course_management/Pages/login.dart';
 import 'package:elective_course_management/Pages/register.dart';
 import 'package:elective_course_management/Pages/searchPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  });
   runApp(
     GetMaterialApp(
       title: "Elective Course Management",
