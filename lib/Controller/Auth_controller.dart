01de4dadd70c394adae843f7d2fed721ec4f26f3
Aric <aricvasaya10@gmail.com>
2025-07-04 19:33:24 +0530

    Add Controller

--- lib/Controller/Auth_controller.dart
+++ lib/Controller/Auth_controller.dart
@@ -1,5 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
-  void signUp() {}
+  void signUp(String email, String password, String rollNo) async {
+    try {
+      if (email.isNotEmpty && password.isNotEmpty && rollNo.isNotEmpty) {
+        UserCredential credential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password);
+      }
+    } catch (e) {
+      Get.snackbar("Error Occured :", e.toString());
+    }
+  }
 }
